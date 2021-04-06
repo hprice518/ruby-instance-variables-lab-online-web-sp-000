ee40ed89a83645bebf7b4ac5186aa0f96da00a31
Harrison Price <hprice518@gmail.com>
2021-04-06 19:25:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 
 class Dog 
   
-  def name =(dog_name)
+  def name= (dog_name)
     this_dogs_name = dog_name
   end
   
@@ -9,4 +9,14 @@ class Dog
     this_dogs_name
   end
   
+end
+
+class Dog
+  def name=(dog_name)
+    this_dogs_name = dog_name
+  end
+ 
+  def name
+    this_dogs_name
+  end
 end
